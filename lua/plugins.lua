318e5ac322e072d665d0002b3d813cf994ad8cea
Felipe Silveira <felipessilveira@gmail.com>
2024-05-22 18:25:27 -0300

    Auto completion

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -116,6 +116,75 @@ local function dap_config()
     }
 end
 
+local function cmp_config()
+    local cmp = require('cmp')
+
+    cmp.setup({
+        snippet = {
+            expand = function(args)
+                vim.fn["vsnip#anonymous"](args.body)
+            end,
+        },
+        window = {
+            -- completion = cmp.config.window.bordered(),
+            -- documentation = cmp.config.window.bordered(),
+        },
+        mapping = cmp.mapping.preset.insert({
+            ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+            ['<C-f>'] = cmp.mapping.scroll_docs(4),
+            ['<C-Space>'] = cmp.mapping.complete(),
+            ['<C-e>'] = cmp.mapping.abort(),
+            ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        }),
+        sources = cmp.config.sources(
+            {
+                { name = 'nvim_lsp' },
+                { name = 'vsnip' },
+            },
+            {
+                { name = 'buffer' },
+            })
+    })
+
+    -- To use git you need to install the plugin petertriho/cmp-git and uncomment lines below
+    -- Set configuration for specific filetype.
+    --[[ cmp.setup.filetype('gitcommit', {
+    sources = cmp.config.sources({
+      { name = 'git' },
+    }, {
+      { name = 'buffer' },
+    })
+ })
+ require("cmp_git").setup() ]]
+    --
+
+    -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+    cmp.setup.cmdline({ '/', '?' }, {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+            { name = 'buffer' }
+        }
+    })
+
+    -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+    cmp.setup.cmdline(':', {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+            { name = 'path' }
+        }, {
+            { name = 'cmdline' }
+        }),
+        matching = { disallow_symbol_nonprefix_matching = false }
+    })
+
+    -- Set up lspconfig.
+    -- local capabilities = require('cmp_nvim_lsp').default_capabilities()
+    -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+    -- require('lspconfig')['rust'].setup {
+    --    capabilities = capabilities
+    -- }
+end
+
 require("lazy-bootstrap").setup({
     {
         "nvim-treesitter/nvim-treesitter",
@@ -179,4 +248,15 @@ require("lazy-bootstrap").setup({
         config = telescope_config,
     },
 
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-path',
+    'hrsh7th/cmp-cmdline',
+    {
+        'hrsh7th/nvim-cmp',
+        config = cmp_config,
+        dependencies = {"neovim/nvim-lspconfig"},
+    },
+    'hrsh7th/cmp-vsnip',
+    'hrsh7th/vim-vsnip',
 })
